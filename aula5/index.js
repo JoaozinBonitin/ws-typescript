3170625a4ad0e289396705d6dfda88a26234a042
JoaozinBonitin <joaozinhomota9@gmail.com>
2024-09-05 01:13:44 -0300

    variaveis let

---
+++ aula5/index.js
@@ -0,0 +1,15 @@
+let nome; // Declaração da variavel, não pode redeclarar variaveis
+nome = 'João'; // Inicializando a variavel
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+// não é possivel declarar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+let nomeCliente = 'Pedro';
+console.log(nomeCliente);
+//não pode criar o nome de uma variavel com um numero
+// não pode conter - e nem caracteres especiais, utilizamos Camel Case para nomes compostos
+// NÃO UTILIZE VAR, SEMPRE UTILIZE LET
+let nomeCompletoDoCliente = 'João Pedro Mota';
+
+console.log(nomeCompletoDoCliente);
