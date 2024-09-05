06c42df2955198b3cdba7352977824cbe3e4b13f
JoaozinBonitin <joaozinhomota9@gmail.com>
2024-09-04 23:34:46 -0300

    first commit

---
+++ aula2/exercicios/exercicio.js
@@ -0,0 +1,1 @@
+console.log(`Meu nome é "NOME". estou aprendendo JavaScript as 10 da manhã`)
---
+++ aula2/index.js
@@ -0,0 +1,5 @@
+console.log('João Pedro Mota'); // String
+console.log("João Pedro Mota");
+console.log(`João Pedro Mota`);
+
+console.log(123456, 35.85, 'João Pedro Mota de novo'); // Number = Número, sem separação de ponto flutuante
