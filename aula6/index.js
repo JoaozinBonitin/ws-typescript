098e87d126578309f52cc770f315bdddbeda66e7
JoaozinBonitin <joaozinhomota9@gmail.com>
2024-09-05 01:28:26 -0300

    constantes em js

---
+++ aula6/index.js
@@ -0,0 +1,19 @@
+// Declaração da variavel, não pode redeclarar variaveis
+// não é possivel declarar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+//não pode criar o nome de uma variavel com um numero
+// não pode conter - e nem caracteres especiais, utilizamos Camel Case para nomes compostos
+// NÃO UTILIZE VAR, SEMPRE UTILIZE LET
+// não modificar o valor de uma constante
+// linguagem de tipagem dinamica
+const nome = 'João';
+//console.log(nome);
+
+// String = Texto | Number = Número
+const primeiroNumero = 5; // Number
+const segundoNumero = 10; // number
+const resultado = primeiroNumero * segundoNumero;
+const resultaDuplicado = resultado * 2;
+
+// tipo da constante ou variável
+console.log(typeof primeiroNumero);
