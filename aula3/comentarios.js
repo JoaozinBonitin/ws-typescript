4b4a09ea2382e6f088727f1614025c5020b3aaf4
JoaozinBonitin <joaozinhomota9@gmail.com>
2024-09-04 23:42:53 -0300

    comentarios em js

---
+++ aula3/comentarios.js
@@ -0,0 +1,12 @@
+// exemplo de comentário
+console.log('Olá Mundo!'); // outro exemplo de comentário
+console.log('Linha1');
+// comentário -  mais um exemplo
+console.log('Linha2');
+console.log('Linha3');
+console.log('Linha4');
+//vai ser ignorado
+/*
+Exemplo de 
+comentário em linha
+*/
