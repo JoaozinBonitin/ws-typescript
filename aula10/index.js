cdc843802a1b3965c8a57af271f2969224bf275c
JoaozinBonitin <joaozinhomota9@gmail.com>
2024-09-16 16:15:55 -0300

    operadores aritmeticos, confirm e prompt

---
+++ aula10/index.js
@@ -0,0 +1,46 @@
+/* 
+Aritméticos 
++ Adção / concatenação
+- Subtração
+/ Divisão
+* Multiplicação
+** Potenciação
+% modulo -> pegar o valor do resto de uma Divisão
+
+*/
+
+// const num1 = 5;
+// const num2 = 10;
+// console.log(num1/num2);
+
+// let contador = 1;
+
+// contador++; // 2
+// contador++; // 3
+// contador++; // 4 
+// contador++; // 5
+
+// console.log(contador++); // pós incremento
+// console.log(++contador); // pré incremento
+
+// Operadore de atribuição
+// Incremento = ++
+// Dcremento = --
+
+// fazer o incrimento de mais de um numero
+
+// const passo = 2;
+// let contador = 0;
+
+// contador += passo; // contador = contador  50
+// console.log(contador);
+// contador += passo;
+// console.log(contador);
+// contador += passo;
+// console.log(contador);
+
+const num1 = 10;
+const num2 = 'Luiz'; // esta tentando somar um numero com uma String
+
+// erro NaN (not a number) - parseInt(Inteiro), parseFloat(Decimais)
+console.log(num1 * num2);
